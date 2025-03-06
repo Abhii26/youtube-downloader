e001c612e40703e55ae48e80802d086e7f085d75
Abhii <abhishektiwari7065@gmail.com>
2025-03-06 12:12:11 +0530

    my commit

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ app.post('/download', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT  || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
